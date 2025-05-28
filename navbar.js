306281a43d8e723dce906d35c9cfc36a5b3d9fde
AHMET EMIRHAN KORKMAZ <EXT03426951@turkcell.entp.tgc>
2025-05-28 17:58:13 +0300

    add 2 pages

--- navbar.js
+++ navbar.js
@@ -1,15 +1,15 @@
 function createNavbar() {
     const navbarHTML = `
         <nav class="navbar">
-            <a class="noact" href="">
+            <a class="noact" href="index.html">
                 <img class="navlogo" src="https://raw.githubusercontent.com/oceandi/links/refs/heads/main/ISP/images/homepage/logo.png">
             </a>
             <div class="nwa">
                 <ul class="nav-menu">
-                    <li><a href="" draggable="false">Router</a></li>
-                    <li><a href="" draggable="false">AP</a></li>
-                    <li><a href="" draggable="false">Powerline</a></li>
-                    <li><a href="" draggable="false">Mesh</a></li>
+                    <li><a href="router.html" draggable="false">Router</a></li>
+                    <li><a href="ap.html" draggable="false">AP</a></li>
+                    <li><a href="powerline.html" draggable="false">Powerline</a></li>
+                    <li><a href="mesh.html" draggable="false">Mesh</a></li>
                     <li><a href="tel:+905321583184" draggable="false">Destek</a></li>
                 </ul>
             </div>
